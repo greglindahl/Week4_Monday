e4b1cdcac4c4f14616ff77daacf06dd42a9b70b4
greglindahl <greglindahl410@gmail.com>
2015-10-05 21:49:01 -0600

    Worked on 1-7

--- jQuerying.js
+++ jQuerying.js
@@ -1,1 +1,31 @@
+$(document).ready(function($) {
+	
+	$('#hover').mouseenter(function() {
+		$('#hover').css('background-color', 'red');
+	})
+	.click(function() {
+		$('#hover').toggleClass('green')
+	})
+	.mouseleave(function() {
+		$('#hover').css('background-color', '#39B7CD')
+	});
 
+	$('.hideMe').click(function() {
+		$('.hideMe').hide();
+	});
+
+	$('.changeColor').click(function() {
+		$('ul li:nth-child(3)').css('color', 'red');
+	});
+
+	$('#removeLi').click(function() {
+		$('ul li:nth-child(2)').hide();
+	});
+
+	$('#removeLi').append('<li>This item is added</li>');
+
+	$( ".cute" ).click(function () {
+		$('.cute').clone().appendTo( ".clones" )
+	});
+
+});
